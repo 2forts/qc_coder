cc3205d1b8fe874c59726a1facb5b631340a728d
Francisco José Orts Gómez <2forts@gmail.com>
2025-08-02 22:42:25 +0200

    Update cirq_translator.py

--- translator/cirq_translator.py
+++ translator/cirq_translator.py
@@ -1,6 +1,6 @@
 from typing import List
 from .base import BaseTranslator
-from qc_coder.gates import Instruction
+from gates import Instruction
 import cirq
 
 class CirqTranslator(BaseTranslator):
