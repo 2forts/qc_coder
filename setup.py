089fdd8450e52652331d43f02efca84cf171333e
Francisco José Orts Gómez <2forts@gmail.com>
2025-08-02 21:12:34 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qc_coder',
+    version='0.1.0',
+    author='Tu Nombre',
+    author_email='tu_email@example.com',
+    description='Método y sistema para codificación eficiente de circuitos cuánticos mediante entrada simbólica',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tu_usuario/qc_coder',
+    packages=find_packages(),
+    install_requires=[
+        'qiskit',
+        'cirq',
+        'amazon-braket-sdk'
+    ],
+    python_requires='>=3.7',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
