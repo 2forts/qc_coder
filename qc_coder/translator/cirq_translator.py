bf928a32f54d561020969225d259f5e3c93ee207
Francisco José Orts Gómez <2forts@gmail.com>
2025-08-02 22:58:52 +0200

    Update and rename translator/cirq_translator.py to qc_coder/translator/cirq_translator.py

--- qc_coder/translator/cirq_translator.py
+++ qc_coder/translator/cirq_translator.py
@@ -1,6 +1,6 @@
 from typing import List
 from .base import BaseTranslator
-from gates import Instruction
+from qc_coder.gates import Instruction
 import cirq
 
 class CirqTranslator(BaseTranslator):
